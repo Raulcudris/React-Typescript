a00e9921eccd10982b78b8704ae1ddc864c8c06f
RaulCudris <raulcudriz@hotmail.com>
2024-03-28 12:52:48 -0500

    first commit

---
+++ mi-app/src/components/Counter.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const Counter = () => {
+
+    const [counter, setCounter] = useState(0);
+    const incrementar = ()=> {
+        setCounter( counter + 1);
+    }
+
+  return (
+    <div className="mt-5">
+        <h3> Counter: useState</h3>  
+        <span> Valor: { counter }</span>     
+        <br/>
+        <button 
+            onClick={ incrementar }
+            className="btn btn-outline-primary mt-2">
+            +1
+        </button>
+
+    </div>
+  )
+}
